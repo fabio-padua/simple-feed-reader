8d0514f11cf6b2a22506a6966008925ab7cc5378
fabiopadua <fabiopadua@microsoft.com>
2023-01-04 17:39:26 -0300

    chore: CodeQL error

--- SimpleFeedReader/Program.cs
+++ SimpleFeedReader/Program.cs
@@ -2,8 +2,12 @@
 namespace SimpleFeedReader;
 public class Program
 {
+
+    const string strCleanPassword = "a#kasdjhfakj0912";
+
     public static void Main(string[] args)
     {
+        if (strCleanPassword=="") return;
         CreateHostBuilder(args).Build().Run();
     }
 
