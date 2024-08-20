35a1261ab245d37eebd10786941a82c74785e774
Fabio Padua <fabiopadua@microsoft.com>
2024-08-20 15:34:43 -0300

    chore: add web job for profile purpouse

---
+++ WebJob/code-optimization-suggestion-web-job/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HeavyArrayForecast;
+
+Console.WriteLine("Initializing...");
+
+do
+{
+
+    Console.WriteLine("");
+    Console.WriteLine("Get wheater...");
+    Console.WriteLine("");
+
+    foreach (var item in WeatherForecastHelper.GetForecasts())
+    {
+        Console.WriteLine(item.Date.ToString() + " " + item.TemperatureC.ToString() + " " + item.Summary.ToString());
+    };
+
+    Console.WriteLine("");
+    Console.WriteLine("Sleeping for 10s...");
+    Console.WriteLine("");
+
+    Thread.Sleep(10000);
+
+} while (true);
